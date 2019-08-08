b6761711a00c2b1874baabdecf55e84a169d649a
Pat Gonzalez <pgonzalez003@gmail.com>
2019-08-07 19:23:33 -0700

    test push

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#titleCont").hide();
+    $("#textCont").hide();
+});
+
+$("#start").on("click", function(){
+    $("#intro").hide();
+    $("#titleCont").show();
+    $("#textCont").show();
+});
+
+$("#submit").on("click", function(){
+    location.reload();
+});  
+
